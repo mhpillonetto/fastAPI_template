25039d4dc1a85b09143ac554a3681dab968ece02
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2023-04-02 12:23:17 -0300

    basic methods

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Hique'}}
+@app.get('/blog')
+def index(limit: Optional[int] = 10, published: Optional[bool] = True
+          ):
+    if published:
+        return {'data': f'{limit} published blog(s) from db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
-@app.get('/about')
-def about():
-    return {'data': {'about': 'page'}}
+
+@app.get('/blog/unplublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(req: Blog):
+    return {'data': f'blog is created with title {req.title}'}

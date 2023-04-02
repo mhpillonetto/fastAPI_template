8b00dca28cf3f39e17fef4e29b6b9f2eeb5a74dc
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2023-04-02 15:46:24 -0300

    crud complete

--- blog/main.py
+++ blog/main.py
@@ -36,5 +36,31 @@ def all(db: Session = Depends(get_db)):
 def show(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} not found')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} not found')
     return blog
+
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='blog not found')
+
+    blog.delete(synchronize_session=False)
+    db.commit()
+
+    return 'ok'
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='blog not found')
+
+    blog.update(request, synchronize_session=False)
+    db.commit()
+    return 'updated'
